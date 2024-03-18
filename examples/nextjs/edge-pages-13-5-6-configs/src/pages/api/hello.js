e389620b8662d7c46c22f882e31ad46a9a7c1ea2
José Filho <jc.bsfilho@gmail.com>
2024-03-18 16:14:51 -0300

    chore: update examples by vulcan

--- examples/nextjs/edge-pages-13-5-6-configs/src/pages/api/hello.js
+++ examples/nextjs/edge-pages-13-5-6-configs/src/pages/api/hello.js
@@ -1,17 +1,21 @@
 export const config = {
-  runtime: "edge",
-};
+  runtime: "edge"
+}
 
 export default async function (req) {
-  let requestHeaders = {};
+  let requestHeaders = {}
 
   req.headers.forEach((value, name) => {
     requestHeaders[name] = value;
-  });
-  return new Response(JSON.stringify({ name: "John Doe", requestHeaders }), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  })
+
+  return new Response(
+    JSON.stringify({ name: 'John Doe', requestHeaders }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  )
 }
